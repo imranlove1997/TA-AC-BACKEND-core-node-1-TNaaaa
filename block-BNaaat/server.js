e5d8da4aaf86e01998b33096d98c6e2a99548030
Imran Khan <imranlove1997@gmail.com>
2021-11-28 19:06:52 +0530

    Complete

---
+++ block-BNaaat/server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    if(req.method === 'GET' && req.url === '/file') {
+        res.setHeader('Content-Type', 'text/html');
+        fs.readFile('./node.html', (err, content) => {
+            if(err) console.log(err);
+            res.end(content);
+        });
+    } else if(req.method === 'GET' && req.url === '/stream') {
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream('./node.html').pipe(res);
+    }
+}
+
+server.listen(5555, () => {
+    console.log('server is listening on port 5555');
+})
